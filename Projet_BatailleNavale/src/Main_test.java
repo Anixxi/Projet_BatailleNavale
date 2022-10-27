3d0306b7fc3642b562d2279517a527a2e9cf09bf
Thanujan THAVARATNAM <84854187+Thanu94@users.noreply.github.com>
2022-10-27 23:24:56 +0200

    Grille avec bateau

---
+++ Projet_BatailleNavale/src/Main_test.java
@@ -0,0 +1,8 @@
+public class Main_test {
+
+    public static void main(String[] args) {
+        Plateau p = new Plateau();
+        //p.getPlateau();
+        p.putBateau(5);
+    }
+}
--- Projet_BatailleNavale/src/Plateau.java
+++ Projet_BatailleNavale/src/Plateau.java
@@ -1,2 +1,39 @@
 public class Plateau {
+
+    private int nbCase = 15;
+    private int ligne = 15;
+    private String grille = "|   "; //la case
+    private String bateau = "| b ";
+    protected int posBateau;
+    int pos = 0;
+
+    public Plateau(){    }
+
+    public String getPlateau(){
+        for(int j = 0; j < ligne ; j++){
+            for(int i = 0 ; i< nbCase; i++){System.out.print(grille);};
+            System.out.print('\n');
+        }
+        return null;
+    }
+
+    public void setPosBateau(int posBateau){
+        this.posBateau = posBateau;
+    }
+    public String putBateau(int posBateau){
+        this.pos = 0;
+        setPosBateau(posBateau);
+        for(int j = 0; j < ligne ; j++){
+            for(int i = 0 ; i< nbCase; i++){
+                if(this.pos == this.posBateau){
+                    System.out.print(bateau);
+                }
+                System.out.print(grille);
+                this.pos++;
+            };
+            System.out.print('\n');
+        }
+        return null;
+    }
+
 }
