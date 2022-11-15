a47f5da4028dcea64fcb719ad2ad228d683e6fe7
Weshweshaso <116729911+Weshweshaso@users.noreply.github.com>
2022-11-15 22:48:42 +0100

    Add joueur & navire

--- Projet_BatailleNavale/src/modele/Joueur.java
+++ Projet_BatailleNavale/src/modele/Joueur.java
@@ -1,4 +1,83 @@
 package modele;
 
+import vue.Plateau;
+
+/**
+ * Cette classe simule un joueur dans le jeu
+ * @author Kichoir, Thanujan, Sofiane, Amudhan
+ */
 public class Joueur {
+    private Plateau plateauJoueur; //Chaque joueur a un plateau de joueur et un plateau d'adversaire.
+    private Navire[] joueurNavires; //Chaque joueur a 5 navires
+
+    /**
+     * Crée un nouvel objet plateau pour le joueur (chaque plateau a 2 pièces : le plateau du joueur et le plateau de l'adversaire).
+     * 5 navires à placer pour le joueur
+     * Initialise le plateau du joueur avec des espaces vides dans son plateau (aucun navire n'est situé pour le moment).
+     */
+    public Joueur() {
+        plateauJoueur = new Plateau();
+        joueurNavires = new Navire[5];
+        for (int i = 0; i < 5; i++) {
+            joueurNavires[i] = new Navire();
+        }
+        plateauJoueur.initPlateau();
+    }
+
+    /**
+     *
+     * @param angleStatut Statut de l'angle pour le navire sélectionné (0 : vertical | 1 : horizontal)
+     * @param taille taille du navire choisi
+     * @param tailleNavire Numéro de navire Peut être parmi {0,1,2,3,4}
+     */
+    public void choixNavire(int angleStatut, int taille, int tailleNavire) {
+        joueurNavires[tailleNavire].setangleStatut(angleStatut); //Définit le statut de l'angle pour le navire choisi
+        joueurNavires[tailleNavire].setColsOrlignes(taille); //Set soit des colonnes ou des lignes pour le navire selon l'état de l'angle
+    }
+
+    /**
+     * Place le navire choisi sur le plateau du joueur.
+     * @param tailleNavire est le numéro du navire choisi. Il peut être compris entre {0,1,2,3,4}.
+     */
+    public void placementNavires(int tailleNavire) {
+        for (int i = joueurNavires[tailleNavire].getpointDepartX(); i < joueurNavires[tailleNavire].getCols() + joueurNavires[tailleNavire].getpointDepartX(); i++) {
+            for (int j = joueurNavires[tailleNavire].getpointDepartY(); j < joueurNavires[tailleNavire].getlignes() + joueurNavires[tailleNavire].getpointDepartY(); j++) {
+                plateauJoueur.placerNavires(i, j); //Met 0 dans la coordonnée donnée, ce qui signifie qu'il y a un navire sur le plateau.
+            }
+        }
+    }
+
+    /**
+     * Vérifier s'il est possible de placer un navire aux coordonnées données ou non
+     * @param x coordonnée x
+     * @param y coordonnée y
+     * @param tailleNavire Numéro du navire que nous voulons vérifier s'il est possible de le placer aux coordonnées données.
+     * Retourne true : Il est possible de placer le vaisseau aux coordonnées données | false : Il n'est pas possible de placer le navire aux coordonnées données.
+     */
+    public boolean checkAvailable(int x, int y, int tailleNavire) {
+        for (int i = joueurNavires[tailleNavire].getpointDepartX(); i < joueurNavires[tailleNavire].getCols() + joueurNavires[tailleNavire].getpointDepartX(); i++) {
+            for (int j = joueurNavires[tailleNavire].getpointDepartY(); j < joueurNavires[tailleNavire].getlignes() + joueurNavires[tailleNavire].getpointDepartY(); j++) {
+                if (i > 9 || i < 0 || j > 9 || j < 0 || plateauJoueur.getPlateauJoueur()[i][j] != -1 ) //Si les coordonnées ne sont pas situées dans le tableau ou si un navire se trouve déjà à l'endroit des coordonnées données, il n'est pas possible d'y placer le navire.
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    /******************** METHODS GETTER ***************************/
+    public void afficherPlateauJoueur() {
+        plateauJoueur.afficherPlateauxJoueur();
+    }
+
+    public void afficherPlateauAdversaire() {
+        plateauJoueur.afficherPlateauAdversaire();
+    }
+
+    public Plateau getPlateauxJoueur() {
+        return plateauJoueur;
+    }
+
+    public Navire[] getJoueurNavires() {
+        return joueurNavires;
+    }
 }
--- Projet_BatailleNavale/src/modele/Navire.java
+++ Projet_BatailleNavale/src/modele/Navire.java
@@ -1,36 +1,95 @@
 package modele;
 
-public abstract class Navire{
+/**
+ * Cette classe simule un navire
+ * @author Kichoir, Thanujan, Sofiane, Amudhan
+ */
+public class Navire {
+    private int angleStatut; //1: horizontal | 0: vertical
+    private int lignes;
+    private int cols;
+    private int pointDepartX;
+    private int pointDepartY;
+    private int taille;
 
-    protected boolean estVivant; //private?
-    protected boolean navireABouger;
-    protected int taille;
-    protected String nom;
-    public Object objectPartage;
-    protected int impacte;
-    protected int pointDeVie; //point de vie
+    private String typeNavire;
 
-    public Navire(){}
 
-    public Navire(boolean vivant){
-        this.estVivant = vivant;
-    }
-    public Navire(Object object) {
-        objectPartage = object;
-        this.estVivant = true;
-    }
 
-    public int getImpacte(){
-        return impacte;
-    }
-
-    public boolean navireABouger() {
-        return this.navireABouger;
+    /**
+     * Définit le statut de l'angle et la taille à zéro par défaut. Cela va changer plus tard
+     */
+    public Navire() {
+        angleStatut = 0;
+        taille = 0;
     }
 
-    public boolean setNavireBouger(boolean aBouger){
-        return this.navireABouger = aBouger;
+    /*************************** LES MÉTHODES GETTER ET SETTER ************************/
+    public int getangleStatut() {
+        return angleStatut;
     }
 
+    public void setangleStatut(int angleStatut) {
+        this.angleStatut = angleStatut;
+    }
 
+    public int getlignes() {
+        return lignes;
+    }
+
+    public void setlignes(int lignes) {
+        this.lignes = lignes;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public void setCols(int cols) {
+        this.cols = cols;
+    }
+
+    public int getpointDepartX() {
+        return pointDepartX;
+    }
+
+    public int getpointDepartY() {
+        return pointDepartY;
+    }
+    /*************************** MÉTHODES GETTER ET SETTER ************************/
+
+
+    /**
+     * Récupère la taille du navire et définit le nombre de colonnes et de lignes du navire en fonction de l'état de l'angle.
+     * @param num Est la taille du navire donnée par l'utilisateur, peut être comprise entre {2,3,4,5}.
+     */
+    public void setColsOrlignes(int num) {
+        if (angleStatut == 0) { //Si le navire est vertical
+            lignes = 1;
+            cols = num;
+            taille = num;
+        } else if (angleStatut == 1) {//Si le navire est horizontal
+            lignes = num;
+            cols = 1;
+            taille = num;
+        }
+    }
+
+    /**
+     * Définit le point de départ du vaisseau (Coordonnées de l'extrême gauche pour le vaisseau horizontal | L'extrême haut pour le vaisseau vertical)
+     * @param x x coordonnée du lieu de départ
+     * @param y coordonnée y du lieu de départ
+     */
+    public void pointDepartNavire(int x, int y) {
+        this.pointDepartX = x;
+        this.pointDepartY = y;
+    }
+
+    /**
+     *
+     * @return taille du navire
+     */
+    public int getTaille() {
+        return taille;
+    }
 }
